10268c755b50dc234b4499251200fe22d0974bb2
Bridget Shannon <bridgetrshannon@gmail.com>
2020-10-19 14:53:51 -0400

    heroku commit

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+var mysql = require("mysql");
+// Global for connection
+var connection;
+// If on Heroku use the Envirnmental Variable for JawsDB
+if (process.env.JAWSDB_URL){
+  connection = mysq.creatConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootroot",
+    database: "burgers_db"
+  });
+};
+ //look for the error
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+module.exports = connection;
